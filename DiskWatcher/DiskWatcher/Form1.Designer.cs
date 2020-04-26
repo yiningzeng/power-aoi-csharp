c81f85e856232ac8b902cf184b6ae7831a282275
baymin <yiningzeng@outlook.com>
2020-04-26 10:22:48 +0800

    feat: 硬盘监控支持自动删除指定文件夹下的过期文件

--- DiskWatcher/DiskWatcher/Form1.Designer.cs
+++ DiskWatcher/DiskWatcher/Form1.Designer.cs
@@ -31,10 +31,13 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerAutoDeletePath = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.手动清理硬盘ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,6 +48,21 @@
             this.notifyIcon1.Text = "硬盘空间监控中...";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.手动清理硬盘ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
             // 
             // button1
             // 
@@ -61,25 +79,35 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // contextMenuStrip1
+            // timerAutoDeletePath
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.退出ToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            this.timerAutoDeletePath.Interval = 43200000;
+            this.timerAutoDeletePath.Tick += new System.EventHandler(this.timerAutoDeletePath_Tick);
             // 
-            // 退出ToolStripMenuItem
+            // button2
             // 
-            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
-            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.退出ToolStripMenuItem.Text = "退出";
-            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            this.button2.Location = new System.Drawing.Point(105, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // 手动清理硬盘ToolStripMenuItem
+            // 
+            this.手动清理硬盘ToolStripMenuItem.Name = "手动清理硬盘ToolStripMenuItem";
+            this.手动清理硬盘ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.手动清理硬盘ToolStripMenuItem.Text = "手动清理硬盘";
+            this.手动清理硬盘ToolStripMenuItem.Click += new System.EventHandler(this.手动清理硬盘ToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(259, 103);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -102,6 +130,9 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Timer timerAutoDeletePath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem 手动清理硬盘ToolStripMenuItem;
     }
 }
 
--- DiskWatcher/DiskWatcher/Form1.cs
+++ DiskWatcher/DiskWatcher/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace DiskWatcher
 
             InitializeComponent();
             timer1.Start();
+            timerAutoDeletePath.Start();
         }
 
         private void notifyIcon1_DoubleClick(object sender, EventArgs e)
@@ -30,6 +32,52 @@ namespace DiskWatcher
             else
             {
                 this.Show();
+            }
+        }
+
+        public void AutoDelete()
+        {
+            try
+            {
+                
+                string disk = ConfigurationManager.AppSettings["AutoDeletePath"];
+                //C#遍历指定文件夹中的所有文件 
+                DirectoryInfo TheFolder = new DirectoryInfo(disk);
+                if (!TheFolder.Exists)
+                    return;
+
+                //遍历文件夹
+                foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(NextFolder.FullName);
+                    DateTime todayZero = Convert.ToDateTime(DateTime.Now.ToString("D").ToString());
+                    if(NextFolder.LastWriteTime < todayZero)
+                    {
+                        Directory.Delete(NextFolder.FullName,true);
+                    }
+                   
+                }
+
+
+                //遍历文件
+                foreach (FileInfo NextFile in TheFolder.GetFiles())
+                {
+                    //if (NextFile.Name == "0-0-11.grid")
+                    //    continue;
+                    // 获取文件完整路径
+                    string heatmappath = NextFile.FullName;
+                    Console.WriteLine(heatmappath);
+                    DateTime todayZero = Convert.ToDateTime(DateTime.Now.ToString("D").ToString());
+                    if (NextFile.LastWriteTime < todayZero)
+                    {
+                        File.Delete(heatmappath);
+                    }
+                }
+            }
+            catch (Exception er)
+            {
+
             }
         }
 
@@ -47,7 +95,10 @@ namespace DiskWatcher
             }
         }
         #endregion
-
+        private void timerAutoDeletePath_Tick(object sender, EventArgs e)
+        {
+            AutoDelete();
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             try
@@ -112,6 +163,16 @@ namespace DiskWatcher
         private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             System.Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AutoDelete();
+        }
+
+        private void 手动清理硬盘ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AutoDelete();
         }
     }
 }
